89fc40f57f2b4c363a3992d7c091a93bcf3ef81c
Naveen Kumar R <159445406+naveenkumar-ramesh2004@users.noreply.github.com>
2025-11-08 19:13:10 +0530

    Create Header,Body(Search bar and Restrocard)

---
+++ Episode 4: Talk is Cheap, Show Me the Code!/Code/App/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import Header from '../Header/Header'
+import Body from '../Body/Body';
+function App() {
+
+  /****
+   * -Header
+   *   - Logo
+   *   - Navigation
+   *      - Home
+   *      - About
+   *      - Contact
+   *      - Cart
+   *   - Login/Signup
+   * 
+   * 
+   * - Body
+   *  - Search Bar
+   * - Restaurant List
+   *   - Restaurant Card
+   *    - Image
+   *    - Name
+   *    - Cuisines
+   *    - Ratings
+   *    - Price Range
+   * 
+   * 
+   * - Footer 
+   *  - Links
+   *  - Contact Info
+   *  - Social Media Icons
+   *  - Copyright Info
+   * 
+   * 
+   */
+
+
+  return (
+    <>
+    <Header />
+    <Body />
+    </>
+  )
+}
+
+export default App;
